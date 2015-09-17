e09ef3f28cfd3e998075ed7266dcad060e804f79
Syu93 <herve.tutuaku@gmail.com>
2015-09-17 04:00:38 +0200

    [feature] start the project
    CD issue to fix

---
+++ service/service.php
@@ -0,0 +1,25 @@
+<?php
+
+function shell() {
+	$cmd = '';
+	if (isset($_POST['cmd']))
+		$cmd = $_POST['cmd'];
+
+	$descriptorspec = array(
+	   0 => array("pipe", "r"),   // stdin is a pipe that the child will read from
+	   1 => array("pipe", "w"),   // stdout is a pipe that the child will write to
+	   2 => array("pipe", "w")    // stderr is a pipe that the child will write to
+	);
+	flush();
+	$process = proc_open($cmd, $descriptorspec, $pipes, realpath('./'), array());
+	$output = "";
+	if (is_resource($process)) {
+	    while ($s = fgets($pipes[1])) {
+	        $output .= $s;
+	        flush();
+	    }
+	}
+	
+	echo json_encode($output);
+	die;
+}
